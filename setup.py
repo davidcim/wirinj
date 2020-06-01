fb27d39da8ed4e387bb58bf47138874bd1b9c66b
davidcim <david@cimaware.com>
2020-06-01 12:50:01 +0200

    'build' directory excluded from from setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/davidcim/wirinj",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=('build*',)),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
